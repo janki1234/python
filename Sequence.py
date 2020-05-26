c61055ff60a18c40694bb463d757dd23f9534b46
janki1234 <61007403+janki1234@users.noreply.github.com>
2020-05-26 17:43:13 +0530

    String,List and tuple added

---
+++ Sequence.py
@@ -0,0 +1,23 @@
+"""Different type of sequences
+@janki jodhani 26-5-2020
+"""
+
+# String
+name="Alice"
+
+#Tuple
+fruits=("Apple","Mango","Orange")
+
+#List
+city=["Amreli","Ahmedabad","Surat","Rajkot"]
+
+#Accessed by index zero for first thing
+
+#To get A from string name
+print(name[0])
+
+#To get Mango from tuple fruit 
+print(fruits[1])
+
+#To get Surat from list city
+print(city[2])
