64d2954774628cd9eea4e298157addd5474de6e9
janki1234 <61007403+janki1234@users.noreply.github.com>
2020-05-26 17:01:26 +0530

    different type of data type of value

---
+++ Variable.py
@@ -0,0 +1,21 @@
+"""In python we can store different data type of value like integer,float,string,boolean and empty"""
+#Integer type
+i=12
+print(f"i is {i}")
+
+#Float type
+f=12.4
+print(f"f is {f}")
+
+#String type
+s="pythonista"
+print(f"s is {s}")
+
+#Boolean type
+b=True
+print(f"b is {b}")
+
+#empty type
+n=None
+print(f"n is {n}")
+
