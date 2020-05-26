dc4e521e482b6837eabd43948efb0913176ca185
janki1234 <61007403+janki1234@users.noreply.github.com>
2020-05-26 17:55:35 +0530

    set and dictionary added

---
+++ Set-dict.py
@@ -0,0 +1,18 @@
+"""Define set and dictionary 
+@janki jodhani 26-05-2020
+"""
+#set
+set={1,2,3,4,5}
+
+#Dictionary declare using key,value pair
+
+std={"Aysha":5,"Aesha":7,"krishna":12}
+
+#get data using key value
+
+print("Aysha in",std["Aysha"],"standard.")
+
+#add data to dictionary
+
+std["Varun"]=9
+print(std)
